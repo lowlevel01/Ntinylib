f23cdd9442a8e1beed5004771c41b154f3a6e27b
Zakariaa <88563786+lowlevel01@users.noreply.github.com>
2025-06-14 00:11:40 +0100

    Update ntinylib.cpp

--- ntinylib.cpp
+++ ntinylib.cpp
@@ -35,23 +35,23 @@ LPVOID MapNtdllFromDisk() {
 
 
 // Returns syscall number for a given function, or 0xFFFF if it is not found (or technically hooked but use the other functionto check)
-WORD GetSyscallNumber(const std::string& funcName) {
-    BYTE* base = (BYTE*) MapNtdllFromDisk();
-    if (!base) return 0xFFFF;
+DWORD GetSyscallNumber(const std::string& funcName) {
+    BYTE* base = (BYTE*)MapNtdllFromDisk();
+    if (!base) return 0xFFFFFFFF;
 
-    auto dos = (IMAGE_DOS_HEADER*)base;
-    auto nt = (IMAGE_NT_HEADERS*)(base + dos->e_lfanew);
+    auto dos = reinterpret_cast<IMAGE_DOS_HEADER*>(base);
+    auto nt = reinterpret_cast<IMAGE_NT_HEADERS*>(base + dos->e_lfanew);
     auto& dir = nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
-    if (!dir.VirtualAddress) { UnmapViewOfFile(base); return 0xFFFF; }
+    if (!dir.VirtualAddress) { UnmapViewOfFile(base); return 0xFFFFFFFF; }
 
-    auto exp = (IMAGE_EXPORT_DIRECTORY*)(base + dir.VirtualAddress);
-    auto names = (DWORD*)(base + exp->AddressOfNames);
-    auto ords = (WORD*)(base + exp->AddressOfNameOrdinals);
-    auto funcs = (DWORD*)(base + exp->AddressOfFunctions);
+    auto exp = reinterpret_cast<IMAGE_EXPORT_DIRECTORY*>(base + dir.VirtualAddress);
+    auto names = reinterpret_cast<DWORD*>(base + exp->AddressOfNames);
+    auto ords = reinterpret_cast<WORD*>(base + exp->AddressOfNameOrdinals);
+    auto funcs = reinterpret_cast<DWORD*>(base + exp->AddressOfFunctions);
 
-    WORD ssn = 0xFFFF;
-    for (DWORD i = 0; i < exp->NumberOfNames; ++i) {
-        std::string name = (char*)(base + names[i]);
+    DWORD syscallNum = 0xFFFFFFFF;
+    for (DWORD i = 0; i < exp->NumberOfNames; i++) {
+        std::string name = reinterpret_cast<char*>(base + names[i]);
         if (_stricmp(name.c_str(), funcName.c_str()) != 0)
             continue;
 
@@ -59,12 +59,13 @@ WORD GetSyscallNumber(const std::string& funcName) {
         // Check if syscall stub is the pattern: 4C 8B D1 B8 ?? ?? 0F 05
         // <=> mov r10, rcx etc...
         if (fn[0] == 0x4C && fn[1] == 0x8B && fn[2] == 0xD1 && fn[3] == 0xB8) {
-            ssn = *(WORD*)(fn + 4);
+            syscallNum = *reinterpret_cast<DWORD*>(fn + 4);
         }
+        break;
     }
 
     UnmapViewOfFile(base);
-    return ssn;
+    return syscallNum;
 }
 
 
