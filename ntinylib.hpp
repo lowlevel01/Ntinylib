42eb5a228e65854007e97d497b50086788555c0b
Zakariaa <88563786+lowlevel01@users.noreply.github.com>
2025-06-13 23:26:29 +0100

    Create ntinylib.hpp

---
+++ ntinylib.hpp
@@ -0,0 +1,24 @@
+#include <windows.h>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <psapi.h>
+#include <memoryapi.h>
+
+struct ExportedFunc {
+    std::string name;
+    DWORD      rva;
+    WORD       ordinal;
+};
+
+struct SyscallEntry {
+    std::string name;
+    WORD        number;
+    BYTE* addr;  // address of stub in base
+};
+
+
+LPVOID MapNtdllFromDisk();
+WORD GetSyscallNumber(const std::string& funcName);
+int IsFunctionHooked(const std::string& funcName);
