bc454a3f871ef36bbf28aab71c94db67eb3fb149
Zakariaa <88563786+lowlevel01@users.noreply.github.com>
2025-06-14 00:20:29 +0100

    Update ntinylib.hpp

--- ntinylib.hpp
+++ ntinylib.hpp
@@ -6,17 +6,6 @@
 #include <psapi.h>
 #include <memoryapi.h>
 
-struct ExportedFunc {
-    std::string name;
-    DWORD      rva;
-    WORD       ordinal;
-};
-
-struct SyscallEntry {
-    std::string name;
-    WORD        number;
-    BYTE* addr;  // address of stub in base
-};
 
 
 LPVOID MapNtdllFromDisk();
